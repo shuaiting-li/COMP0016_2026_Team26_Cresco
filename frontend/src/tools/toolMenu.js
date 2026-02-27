5efeebf5764ad5bd89c907defe450611afa3b02f
VivekVarkey5121 <vivvarkey@gmail.com>
2026-02-27 11:09:46 +0000

    sat-image ui button

--- frontend/src/tools/toolMenu.js
+++ frontend/src/tools/toolMenu.js
@@ -4,6 +4,8 @@ export const STUDIO_ITEMS = [
     { title: "Weather Data", icon: CloudRain },
     { title: "...", icon: TrophyIcon },
     { title: "Yield Forecast", icon: LineChart },
-    { title: "Add Farm", icon: Sprout }
+    { title: "Add Farm", icon: Sprout },
+    { title: "Satellite Imagery", icon: TrophyIcon }
+
     
 ];
